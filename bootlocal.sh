6e7010cc511a57418fbbabbc3090c565af9f3e06
Alban Montaigu <alban.montaigu@gmail.com>
2016-02-07 16:15:50 +0100

    First version

---
+++ bootlocal.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# -------------------------------------------------
+# Parametrization
+# -------------------------------------------------
+BOOT2DOCKER_CONFIG_DIR="/var/lib/boot2docker/config"
+BOOT2DOCKER_PROVISIONING_DIR="$BOOT2DOCKER_CONFIG_DIR/provisioning"
+HOME_DOCKER_USER="/home/docker"
+BOOT2DOCKER_PROVISIONING_HOME_DOCKER_USER_DIR="BOOT2DOCKER_PROVISIONING_DIR$HOME_DOCKER_USER"
+
+# ----------------------------------------------------
+# COPY alias file for docker user
+# ----------------------------------------------------
+echo "== Adding .alias to $HOME_DOCKER_USER !"
+sudo cp -vf $BOOT2DOCKER_PROVISIONING_HOME_DOCKER_USER_DIR/.alias $HOME_DOCKER_USER/.alias
+chown -v docker:staff $HOME_DOCKER_USER/.alias
+echo "== .alias added to $HOME_DOCKER_USER !"
+
+# ----------------------------------------------------
+# ADD complements in .ashrc for docker user
+# ----------------------------------------------------
+echo "== Adding complements to $HOME_DOCKER_USER/.ashrc !"
+cat $BOOT2DOCKER_PROVISIONING_HOME_DOCKER_USER_DIR/.ashrc >> $HOME_DOCKER_USER/.ashrc
+chmod 777 /home/docker/.ashrc
+echo "== Complements added to $HOME_DOCKER_USER/.ashrc !"
---
+++ provision.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# -------------------------------------------------
+# Solves insecure registry (example)
+# -------------------------------------------------
+#if grep -q my.inscure.registry.url /var/lib/boot2docker/profile; then
+#    echo "== nothing todo in boot2docker/profile !"
+#else
+#    echo 'EXTRA_ARGS="--insecure-registry my.inscure.registry.url"' >> /var/lib/boot2docker/profile
+#    echo "== boot2docker/profile updated with registry !"
+#fi
+
+# -------------------------------------------------
+# Docker profile customization (example)
+# -------------------------------------------------
+#if grep -q my_option /var/lib/boot2docker/profile; then
+#    echo "== nothing todo in boot2docker/profile for my_key_command !"
+#else
+#    echo -e '\nDOCKER_OPTS=my_option\n' >> /var/lib/boot2docker/profile
+#    echo "== boot2docker/profile updated with my_option !"
+#fi
