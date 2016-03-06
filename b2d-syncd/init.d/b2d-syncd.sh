9393b015d9dc4fd71437e9eab2e6914a4b1e525f
Alban Montaigu <alban.montaigu@gmail.com>
2016-03-06 01:08:21 +0100

    Try to have daemon working

--- b2d-syncd/init.d/b2d-syncd.sh
+++ b2d-syncd/init.d/b2d-syncd.sh
@@ -9,25 +9,43 @@ DAEMON_DIR="/var/lib/boot2docker/config/b2d_syncd"
 DAEMON_BIN="$DAEMON_DIR/bin/b2d_syncd.sh"
 DAEMON_USER="root"
 DAEMON_NAME="b2d-syncd"
+DAEMON_PID_FILE="/var/run/b2d-sync.pid"
 
 # Test binary existence
-test -x $DAEMON_BIN || exit 0
+test -x $DAEMON_BIN || exit 1
 
 # Daemon start
 b2d_syncd_start () {
     echo "Starting system $DAEMON_NAME daemon"
-    start-stop-daemon --background --name $DAEMON_NAME --start --quiet --chuid $DAEMON_USER --startas $DAEMON_BIN
+    $DAEMON_BIN &
+    echo "$!" > $DAEMON_PID_FILE
 }
 
 # Daemon stop
 b2d_syncd_stop () {
     echo "Stopping system $DAEMON_NAME daemon"
-    start-stop-daemon --name $DAEMON_NAME --stop --retry 5 --quiet --name $DAEMON_NAME
+    kill $(cat $DAEMON_PID_FILE)
+}
+
+# Checks daemon
+b2d_syncd_check() {
+    [ -f $DAEMON_PID_FILE ] && ps -A -o pid | grep "^\s*$(cat $DAEMON_PID_FILE)$" > /dev/null 2>&1
+}
+
+# Show daemon status
+b2d_syncd_status() {
+    if check; then
+        echo "$DAEMON_NAME daemon is running"
+        exit 0
+    else
+        echo "$DAEMON_NAME daemon is not running"
+        exit 1
+    fi
 }
 
 # Help on usage
 b2d_syncd_usage(){
-    echo "Usage: $DAEMON_BIN start/stop"
+    echo "Usage: $DAEMON_BIN start/stop/status"
     echo
     echo "Starts or stop a small b2d sync daemon that sync every 2 seconds."
 }
@@ -37,6 +55,8 @@ case "$1" in
     start) b2d_syncd_start
         ;;
     stop) b2d_syncd_stop
+        ;;
+    status) b2d_syncd_status
         ;;
     *) b2d_syncd_usage
         ;;
