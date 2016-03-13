e660676142c3892bce88d3a22da0f570c79fc3a9
Alban Montaigu <alban.montaigu@gmail.com>
2016-03-13 13:04:41 +0100

    Introducing persistent param change system

---
+++ param.d/README.md
@@ -0,0 +1,5 @@
+# Param placeholder
+
+All persistent param are stored here.
+
+This system enable to change the value in a simple with small ```Vagrantfile``` tuning.
---
+++ param.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# ===================================================================
+# Provisionning script executed during vagrant provision to
+# change persistent param located in param dir.
+# ===================================================================
+
+
+# -------------------------------------------------------------------
+# Parametrization (for all environment)
+# -------------------------------------------------------------------
+export BOOT2DOCKER_EXTENSION_DIR="/var/lib/boot2docker/extension"
+export BOOT2DOCKER_EXTENSION_PARAM_DIR="$BOOT2DOCKER_EXTENSION_DIR/param"
+
+
+# -------------------------------------------------------------------
+# Change param values if possible
+# -------------------------------------------------------------------
+if [ -f "$BOOT2DOCKER_EXTENSION_PARAM_DIR/$1" ]; then
+    echo "$2" > "$1"
+fi
