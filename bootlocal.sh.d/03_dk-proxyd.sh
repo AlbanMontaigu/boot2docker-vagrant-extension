d66e054cf7a4450ef33d19abb20ac7a26cce1200
alban.montaigu <alban.montaigu@gmail.com>
2016-10-31 18:11:05 +0100

    Now proxyd shlould be started if enabled in status env var

---
+++ bootlocal.sh.d/03_dk-proxyd.sh
@@ -0,0 +1,15 @@
+
+# ===================================================================
+# dk-proxyd daemon lunching
+# ===================================================================
+
+# Get daemon status param
+export DK_PROXYD_STATUS=$(cat "$BOOT2DOCKER_EXTENSION_PARAM_DIR/DK_PROXYD_STATUS")
+
+# Check if the daemon is enabled
+if [[ "$DKTB_EXTENSION_STATUS" == "ON" ]]; then
+    echo "== Starting dk-proxyd daemon !"
+    $BOOT2DOCKER_EXTENSION_DIR/daemons/dk-proxyd/init.d/dk-proxyd.sh start
+else
+    echo "== dk-proxyd daemon not started !"
+fi
