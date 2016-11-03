78d518b7205bac8df467cb65c7e22a709c480901
alban.montaigu <alban.montaigu@gmail.com>
2016-11-03 13:02:42 +0100

    Better log + typo in sample

--- provision.sh.d/01_docker.sh
+++ provision.sh.d/01_docker.sh
@@ -8,10 +8,10 @@
 # Solves insecure registry (example)
 # -------------------------------------------------------------------
 #if grep -q my.inscure.registry.url /var/lib/boot2docker/profile; then
-#    echo "== nothing todo in boot2docker/profile !"
+#    echo "[INFO] Nothing todo in boot2docker/profile !"
 #else
-#    echo 'EXTRA_ARGS="--insecure-registry my.inscure.registry.url"' >> /var/lib/boot2docker/profile
-#    echo "== boot2docker/profile updated with registry !"
+#    echo 'EXTRA_ARGS="--insecure-registry my.insecure.registry.url"' >> /var/lib/boot2docker/profile
+#    echo "[INFO] Boot2docker/profile updated with registry !"
 #fi
 
 
@@ -19,8 +19,8 @@
 # Docker profile customization (example)
 # -------------------------------------------------------------------
 #if grep -q my_option /var/lib/boot2docker/profile; then
-#    echo "== nothing todo in boot2docker/profile for my_key_command !"
+#    echo "[INFO] Nothing todo in boot2docker/profile for my_key_command !"
 #else
 #    echo -e '\nDOCKER_OPTS=my_option\n' >> /var/lib/boot2docker/profile
-#    echo "== boot2docker/profile updated with my_option !"
+#    echo "[INFO] Boot2docker/profile updated with my_option !"
 #fi
