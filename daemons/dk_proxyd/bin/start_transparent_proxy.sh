fcd67f934de34a43f5dcc638a1008401e998d20e
Alban Montaigu <alban.montaigu@gmail.com>
2016-11-21 17:57:10 +0100

    Rollback all proxy modif

--- daemons/dk_proxyd/bin/start_transparent_proxy.sh
+++ daemons/dk_proxyd/bin/start_transparent_proxy.sh
@@ -3,9 +3,6 @@
 # Error handling
 set -e
 
-# Load env variables
-source /etc/environment
-
 # Stop and delete existing container
 echo "[INFO] Deleting existing transparent proxy container and network rules ..."
 docker rm -f transparent-proxy 2>/dev/null || true
@@ -14,42 +11,17 @@ docker rm -f transparent-proxy 2>/dev/null || true
 echo "[INFO] Set networking rules ..."
 /usr/local/sbin/iptables -t nat -N TRANSPROXY 2>/dev/null || true
 
-# Detect proxy on host machine listening on 8888 (typically fiddler on windows) to act as a global proxy gateway
-if [[ "$(echo ${http_proxy})" == "http://10.0.2.2:8888" ]]; then
+# We then told iptables to redirect all port 80 connections to the http-relay port
+/usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp --dport 80 -j REDIRECT --to-ports 3128 -w 2>/dev/null || \
+/usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp --dport 80 -j REDIRECT --to-ports 3128 -w
 
-    # Info
-    echo "[INFO] Proxy detected on host machine, using it instead of internals..."
-
-    # and all other connections to the http-connect port.
-    /usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp --dport 80 -j DNAT --to-destination 10.0.2.2:8888 -w 2>/dev/null || \
-    /usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp --dport 80 -j DNAT --to-destination 10.0.2.2:8888 -w
-
-    # and all other connections ah sock like proxy (useful for https handshake that not work with http proxy)
-    /usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp -j DNAT --to-destination 10.0.2.2:8889 -w 2>/dev/null || \
-    /usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp -j DNAT --to-destination 10.0.2.2:8889 -w
-else
-
-    # Info
-    echo "[INFO] Using classic transparent proxy service..."
-
-    # We then told iptables to redirect all port 80 connections to the http-relay port
-    /usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp --dport 80 -j REDIRECT --to-ports 3128 -w 2>/dev/null || \
-    /usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp --dport 80 -j REDIRECT --to-ports 3128 -w
-
-    # and all other connections to the http-connect port.
-    /usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp -j REDIRECT --to-ports 12345 -w 2>/dev/null || \
-    /usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp -j REDIRECT --to-ports 12345 -w
-fi
+# and all other connections to the http-connect port.
+/usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp -j REDIRECT --to-ports 12345 -w 2>/dev/null || \
+/usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp -j REDIRECT --to-ports 12345 -w
 
 # Finally we tell iptables to use the ‘TRANSPROXY’ chain for all outgoing connection in docker containers
 /usr/local/sbin/iptables -t nat -C PREROUTING -p tcp -j TRANSPROXY -m addrtype --dst-type UNICAST ! --dst 172.0.0.0/8 2>/dev/null || \
 /usr/local/sbin/iptables -t nat -A PREROUTING -p tcp -j TRANSPROXY -m addrtype --dst-type UNICAST ! --dst 172.0.0.0/8
-
-# If proxy listening on host machine don't lunch something else
-if [[ "$(echo ${http_proxy})" == "http://10.0.2.2:8888" ]]; then
-    echo "[INFO] Transparent proxy not loaded since proxy detected on host machine"
-    exit 0
-fi
 
 # Try to auto detect region profile by checking proxy value
 PROXY_FILE=$(grep -l -m 1 $BOOT2DOCKER_EXTENSION_DIR/proxy/* -e "${http_proxy:-_NA_}" | head -1)
