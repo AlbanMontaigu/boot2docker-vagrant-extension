a8f292b73d8eed939b0b36ec1e1d49bf05faa855
Alban Montaigu <alban.montaigu@gmail.com>
2016-11-21 12:33:49 +0100

    Fix transparent proxy management

--- daemons/dk_proxyd/bin/start_transparent_proxy.sh
+++ daemons/dk_proxyd/bin/start_transparent_proxy.sh
@@ -15,28 +15,34 @@ echo "[INFO] Set networking rules ..."
 /usr/local/sbin/iptables -t nat -N TRANSPROXY 2>/dev/null || true
 
 # Detect proxy on host machine listening on 8888 (typically fiddler on windows) to act as a global proxy gateway
-prx_port_http="3128"
-prx_port_others="12345"
 if [[ "$(echo ${http_proxy})" == "http://10.0.2.2:8888" ]]; then
+
+    # Info
     echo "[INFO] Proxy detected on host machine, using it instead of internals..."
-    prx_port_http="8888"
-    prx_port_others="8888"
+
+    # and all other connections to the http-connect port.
+    /usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp -j REDIRECT -j DNAT --to-destination 10.0.2.2:8888 2>/dev/null || \
+    /usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp -j REDIRECT -j DNAT --to-destination 10.0.2.2:8888 -w
+else
+
+    # Info
+    echo "[INFO] Using classic transparent proxy service..."
+
+    # We then told iptables to redirect all port 80 connections to the http-relay port
+    /usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp --dport 80 -j REDIRECT --to-ports 3128 -w 2>/dev/null || \
+    /usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp --dport 80 -j REDIRECT --to-ports 3128 -w
+
+    # and all other connections to the http-connect port.
+    /usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp -j REDIRECT --to-ports 12345 -w 2>/dev/null || \
+    /usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp -j REDIRECT --to-ports 12345 -w
 fi
-
-# We then told iptables to redirect all port 80 connections to the http-relay port
-/usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp --dport 80 -j REDIRECT --to-ports ${prx_port_http} -w 2>/dev/null || \
-/usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp --dport 80 -j REDIRECT --to-ports ${prx_port_http} -w
-
-# and all other connections to the http-connect port.
-/usr/local/sbin/iptables -t nat -C TRANSPROXY -p tcp -j REDIRECT --to-ports ${prx_port_others} -w 2>/dev/null || \
-/usr/local/sbin/iptables -t nat -A TRANSPROXY -p tcp -j REDIRECT --to-ports ${prx_port_others} -w
 
 # Finally we tell iptables to use the ‘TRANSPROXY’ chain for all outgoing connection in docker containers
 /usr/local/sbin/iptables -t nat -C PREROUTING -p tcp -j TRANSPROXY -m addrtype --dst-type UNICAST ! --dst 172.0.0.0/8 2>/dev/null || \
 /usr/local/sbin/iptables -t nat -A PREROUTING -p tcp -j TRANSPROXY -m addrtype --dst-type UNICAST ! --dst 172.0.0.0/8
 
 # If proxy listening on host machine don't lunch something else
-if [[ "${prx_port_http}" == "8888" ]]; then
+if [[ "$(echo ${http_proxy})" == "http://10.0.2.2:8888" ]]; then
     echo "[INFO] Transparent proxy not loaded since proxy detected on host machine"
     exit 0
 fi
@@ -52,15 +58,15 @@ CUSTOM_PROXY_PAC_URL_FILE="${CUSTOM_PROXY_PAC}.url"
 if [ -f "${CUSTOM_PROXY_PAC}" ]; then
     # run transparent with custom config
     echo "[INFO] Use your ${CUSTOM_PROXY_PAC}"
-    docker run --name=transparent-proxy --net=host -v $CUSTOM_PROXY_PAC:/mnt/proxy.pac -d amontaigu/transparent-proxy:0.1.0 file:///mnt/proxy.pac
+    docker run --name=transparent-proxy --net=host -v $CUSTOM_PROXY_PAC:/mnt/proxy.pac -d registry-testing.kazan.atosworldline.com/docker-reg/awl-transparent-proxy file:///mnt/proxy.pac
 elif [ -f "${CUSTOM_PROXY_PAC_URL}" ]; then
     echo "[INFO] Use your ${CUSTOM_PROXY_PAC_URL_FILE}"
     CUSTOM_PROXY_PAC_URL="$(cat $CUSTOM_PROXY_PAC_URL_FILE)"
-    docker run --name=transparent-proxy --net=host -d amontaigu/transparent-proxy:0.1.0 "${CUSTOM_PROXY_PAC_URL}"
+    docker run --name=transparent-proxy --net=host -d registry.kazan.atosworldline.com/docker-reg/awl-transparent-proxy "${CUSTOM_PROXY_PAC_URL}"
 else
     # run transparent proxy using wpad
     echo "[INFO] Use proxy set in default auto detected proxy pac"
-    docker run --name=transparent-proxy --net=host -d amontaigu/transparent-proxy:0.1.0 http://wpad/wpad.dat
+    docker run --name=transparent-proxy --net=host -d registry.kazan.atosworldline.com/docker-reg/awl-transparent-proxy http://wpad/wpad.dat
 fi
 echo "[INFO] OK, transparent proxy is started"
 
