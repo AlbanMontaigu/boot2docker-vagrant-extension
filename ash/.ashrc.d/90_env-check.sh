a2fd7d2562f9f5a853809581418d58441b006c20
Alban Montaigu <alban.montaigu@gmail.com>
2017-10-09 16:34:19 +0200

    Better message display

--- ash/.ashrc.d/90_env-check.sh
+++ ash/.ashrc.d/90_env-check.sh
@@ -11,6 +11,16 @@
 # ------------------------------------------------------------
 version_check(){
 
+# To gain visibility for the message
+cat << EOF
+ __     __            _                ____ _               _
+ \ \   / /__ _ __ ___(_) ___  _ __    / ___| |__   ___  ___| | __
+  \ \ / / _ \ '__/ __| |/ _ \| '_ \  | |   | '_ \ / _ \/ __| |/ /
+   \ V /  __/ |  \__ \ | (_) | | | | | |___| | | |  __/ (__|   <
+    \_/ \___|_|  |___/_|\___/|_| |_|  \____|_| |_|\___|\___|_|\_\
+
+EOF
+
     # Get remote version (latest)
     B2D_EXT_LATEST_VERSION=$(curl -m15 -skL https://raw.githubusercontent.com/AlbanMontaigu/boot2docker-vagrant-extension/latest/VERSION)
 
@@ -21,10 +31,10 @@ version_check(){
 
     # Display information
     if ([[ "${B2D_EXT_LATEST_VERSION}" == "${B2D_EXTENSION_VERSION}" ]]); then
-        echo "[INFO] Congratulations ! You have the last version of boot2docker extension (${B2D_EXTENSION_VERSION})"
+        echo -e "[INFO] Congratulations ! You have the last version of boot2docker extension (${B2D_EXTENSION_VERSION})\n\n"
         return 0
     else
-        echo "[INFO] You may consider checking your boot2docker extension version (yours=${B2D_EXTENSION_VERSION}, latest=${B2D_EXT_LATEST_VERSION})"
+        echo -e "[INFO] You may consider checking your boot2docker extension version (yours=${B2D_EXTENSION_VERSION}, latest=${B2D_EXT_LATEST_VERSION})\n\n"
         return 2
     fi
 }
